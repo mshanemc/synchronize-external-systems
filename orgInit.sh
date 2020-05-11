eba6044a7bf1b6757e035177297366cccf825288
mshanemc <shane.mclaughlin@salesforce.com>
2020-05-11 15:52:13 -0500

    script password stuf

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n demoPerms
 sfdx automig:load -d data
 sfdx shane:data:favorite --object Account --where "Name = 'That Account We\'ve Been Working On Forever'"
+sfdx shane:user:password:set -p sfdx1234 -l User -g User
 sfdx force:org:open -p /lightning/o/Account/list?filterName=Recent
