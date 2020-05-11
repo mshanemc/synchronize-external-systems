151cd94575c2aeb7c56c97853aac3e2ab28fae34
mshanemc <shane.mclaughlin@salesforce.com>
2020-05-11 14:22:18 -0500

    fields, event, PB in org

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
-sfdx force:org:open
+sfdx force:user:permset:assign -n demoPerms
+sfdx automig:load -d data
+sfdx shane:data:favorite --object Account --where "Name = 'That Account We\'ve Been Working On Forever'"
+sfdx force:org:open -p /lightning/o/Account/list?filterName=Recent
